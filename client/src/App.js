354a3242a691993d21ee2eff90f8fa1bc9055b53
BNMoyers <50836425+BNMoyers@users.noreply.github.com>
2019-09-16 15:44:50 -0500

    stretch goals complete; saved movies functionality implemented and linked

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   const [savedList, setSavedList] = useState([]);
   
 
-  const addToSavedList = movie => {
+  const addToSavedList = (movie) => {
     setSavedList([...savedList, movie]);
   };
 
@@ -16,7 +16,12 @@ const App = () => {
     <div>
       <SavedList list={savedList} />
       <Route exact path="/" component={MovieList} />
-      <Route path="/movies/:id" component={Movie} />
+      <Route path="/movies/:id" render={(props)=>
+                                          (<Movie
+                                              {...props}
+                                              addToSavedList={addToSavedList}
+                                              savedList={savedList}/>
+                                              )}/>
     </div>
   );
 };
